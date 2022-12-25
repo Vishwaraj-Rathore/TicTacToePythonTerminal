ca1b2c34518a831677a222fdfc1d7ea28a2e2057
Vishwaraj Rathore <vishwaraj3003@gmail.com>
2022-12-25 10:10:30 +0530

    Added printBoard function

---
+++ Main.py
@@ -0,0 +1,9 @@
+currentBoard = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+
+def printBoard():
+    for i in range(3):
+        for j in range(3):
+            print(currentBoard[i][j], " |" if j!=2 else " ", end="")
+        print("\n - | - | - " if i!=2 else "")
+
+printBoard()
