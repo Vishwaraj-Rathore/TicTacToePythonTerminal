4c79997e9dacae0bd80b634fbca1395f0adf7a85
Rathore <vishwaraj.rathore@capgemini.com>
2022-12-26 10:25:53 +0530

    Added computer plays according to what player chooses for X or O

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+import random
+
 currentBoard = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
 positionsLeft = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 userChoice = 1
@@ -30,25 +32,24 @@ def UserInput():
             print("Please enter a valid position value.")
             continue
 
-def GetRowNum():
-    global userChoice
-    if(userChoice < 4):
+def GetRowNum(k):
+    if(k < 4):
         return 0
-    elif(userChoice < 7):
+    elif(k < 7):
         return 1
     else:
         return 2
 
-def GetAxisForInput():
-    global userChoice, a, b
-    rowNumber = GetRowNum()
-    yAxis = userChoice - a[rowNumber]
+def GetAxisForInput(k):
+    global a, b
+    rowNumber = GetRowNum(k)
+    yAxis = k - a[rowNumber]
     xAxis = a[rowNumber] - b[rowNumber]
     return xAxis, yAxis
 
 def PlayerMove():
     global userChoice, currentBoard, playerChoice
-    xAxis, yAxis = GetAxisForInput()
+    xAxis, yAxis = GetAxisForInput(userChoice)
     currentBoard[xAxis][yAxis] = playerChoice[1]
     positionsLeft.remove(userChoice)
 
@@ -61,16 +62,40 @@ def PlayerChoice():
                 playerChoice[0] = "o"
             else:
                 playerChoice[0] = "x"
+                ComputerMove()
             break
         else:
             print("Entered choice is invalid, please choose either X or O")
             continue
 
+def ComputerMove():
+    global positionsLeft, currentBoard, playerChoice
+    computerChoice = random.choice(positionsLeft)
+    xAxis, yAxis = GetAxisForInput(computerChoice)
+    currentBoard[xAxis][yAxis] = playerChoice[0]
+    positionsLeft.remove(computerChoice)
+
+def BoardCheck():
+    global positionsLeft
+    if(len(positionsLeft) == 0):
+        return True
+    else:
+        return False
+
 PlayerChoice()
 
 while(True):
+    if(BoardCheck()):
+        PrintBoard()
+        print("No more positions left to play")
+        break
     PrintBoard()
     UserInput()
     if(userChoice == 0):
         break
     PlayerMove()
+    if(BoardCheck()):
+        PrintBoard()
+        print("No more positions left to play")
+        break
+    ComputerMove()
