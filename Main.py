bf7211d695cda72bdbadc359c7fb41767ed8cdb7
Rathore <vishwaraj.rathore@capgemini.com>
2022-12-26 11:39:04 +0530

    Added win check, computer plays to win, also computer blocks if user winning

--- Main.py
+++ Main.py
@@ -6,6 +6,9 @@ userChoice = 1
 a = [1, 4, 7]
 b = [1, 3, 5]
 playerChoice = [" ", " "]
+winner = 2
+computerWinMovePref = 0
+computerBlockMovePref = 0
 
 def PrintBoard():
     global currentBoard
@@ -69,11 +72,18 @@ def PlayerChoice():
             continue
 
 def ComputerMove():
-    global positionsLeft, currentBoard, playerChoice
-    computerChoice = random.choice(positionsLeft)
+    global positionsLeft, currentBoard, playerChoice, computerWinMovePref, computerBlockMovePref
+    if(computerWinMovePref == 0 and computerBlockMovePref == 0):
+        computerChoice = random.choice(positionsLeft)
+    else:
+        if(computerWinMovePref != 0):
+            computerChoice = computerWinMovePref
+        else:
+            computerChoice = computerBlockMovePref
     xAxis, yAxis = GetAxisForInput(computerChoice)
     currentBoard[xAxis][yAxis] = playerChoice[0]
     positionsLeft.remove(computerChoice)
+    computerWinMovePref = computerBlockMovePref = 0
 
 def BoardCheck():
     global positionsLeft
@@ -82,20 +92,145 @@ def BoardCheck():
     else:
         return False
 
+def GetItemAtPosition(k):
+    global currentBoard
+    xAxis, yAxis = GetAxisForInput(k)
+    return currentBoard[xAxis][yAxis]
+
+def HorizontalCheck():
+    global winner, playerChoice, computerWinMovePref, computerBlockMovePref
+    for i in range(1, 8, 3):
+        counterComputer = 0
+        counterPlayer = 0
+        positionOfBlank = 0
+        for j in range(3):
+            a = i+j
+            if(GetItemAtPosition(a) == playerChoice[0]):
+                counterComputer += 1
+            elif(GetItemAtPosition(a) == playerChoice[1]):
+                counterPlayer += 1
+            else:
+                positionOfBlank = a
+        if(counterComputer == 3):
+            winner = 0
+            return True
+        elif(counterPlayer == 3):
+            winner = 1
+            return True
+        elif(counterComputer == 2):
+            computerWinMovePref = positionOfBlank
+        elif(counterPlayer == 2):
+            computerBlockMovePref = positionOfBlank
+    return False
+
+def VerticalCheck():
+    global winner, playerChoice, computerWinMovePref, computerBlockMovePref
+    for i in range(1, 4):
+        counterComputer = 0
+        counterPlayer = 0
+        positionOfBlank = 0
+        for j in range(0, 7, 3):
+            a = i+j
+            if(GetItemAtPosition(a) == playerChoice[0]):
+                counterComputer += 1
+            elif(GetItemAtPosition(a) == playerChoice[1]):
+                counterPlayer += 1
+            else:
+                positionOfBlank = a
+        if(counterComputer == 3):
+            winner = 0
+            return True
+        elif(counterPlayer == 3):
+            winner = 1
+            return True
+        elif(counterComputer == 2):
+            computerWinMovePref = positionOfBlank
+        elif(counterPlayer == 2):
+            computerBlockMovePref = positionOfBlank
+    return False
+
+def DiagonalCheck():
+    global winner, playerChoice, computerWinMovePref, computerBlockMovePref
+    for i in [1,3]:
+        counterComputer = 0
+        counterPlayer = 0
+        positionOfBlank = 0
+        if(i == 1):
+            for j in range(0, 9, 4):
+                a = i+j
+                if(GetItemAtPosition(a) == playerChoice[0]):
+                    counterComputer += 1
+                elif(GetItemAtPosition(a) == playerChoice[1]):
+                    counterPlayer += 1
+                else:
+                    positionOfBlank = a
+        else:
+            for j in range(0, 5, 2):
+                a = i+j
+                if(GetItemAtPosition(a) == playerChoice[0]):
+                    counterComputer += 1
+                elif(GetItemAtPosition(a) == playerChoice[1]):
+                    counterPlayer += 1
+                else:
+                    positionOfBlank = a
+        if(counterComputer == 3):
+            winner = 0
+            return True
+        elif(counterPlayer == 3):
+            winner = 1
+            return True
+        elif(counterComputer == 2):
+            computerWinMovePref = positionOfBlank
+        elif(counterPlayer == 2):
+            computerBlockMovePref = positionOfBlank
+    return False
+
+def WinnerCheck():
+    global winner
+    if(HorizontalCheck()):
+        if(winner == 0):
+            PrintBoard()
+            print("Computer Won, better luck next time!")
+            return True
+        elif(winner == 1):
+            PrintBoard()
+            print("Congratulations, You won!")
+            return True
+    elif(VerticalCheck()):
+        if(winner == 0):
+            PrintBoard()
+            print("Computer Won, better luck next time!")
+            return True
+        elif(winner == 1):
+            PrintBoard()
+            print("Congratulations, You won!")
+            return True
+    elif(DiagonalCheck()):
+        if(winner == 0):
+            PrintBoard()
+            print("Computer Won, better luck next time!")
+            return True
+        elif(winner == 1):
+            PrintBoard()
+            print("Congratulations, You won!")
+            return True
+    elif(BoardCheck()):
+        PrintBoard()
+        print("It's a draw!")
+        return True
+    else:
+        return False
+
 PlayerChoice()
 
 while(True):
-    if(BoardCheck()):
-        PrintBoard()
-        print("No more positions left to play")
+    if(WinnerCheck()):
         break
     PrintBoard()
     UserInput()
     if(userChoice == 0):
         break
     PlayerMove()
-    if(BoardCheck()):
-        PrintBoard()
-        print("No more positions left to play")
+    if(WinnerCheck()):
         break
     ComputerMove()
